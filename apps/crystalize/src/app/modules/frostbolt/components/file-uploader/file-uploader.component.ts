81541700d03a3a981e9dfc0a1dd24ecbaa88651a
Frederick Gibeau <frederick.gibeau@nventive.com>
2021-12-07 18:44:11 -0500

    [crystalize] Add file upload to frostbolt module

---
+++ apps/crystalize/src/app/modules/frostbolt/components/file-uploader/file-uploader.component.ts
@@ -0,0 +1,93 @@
+import { Component } from '@angular/core';
+import axios from 'axios';
+
+@Component({
+  selector: 'mihyle-file-uploader',
+  templateUrl: './file-uploader.component.html',
+  styleUrls: ['./file-uploader.component.scss'],
+})
+export class FileUploaderComponent {
+  public parsedText: string | undefined;
+
+  constructor() {
+    this.parsedText = undefined;
+  }
+
+  uploadFile = (event: Event) => {
+    const element = event.currentTarget as HTMLInputElement;
+
+    if (element.files && element.files[0]) {
+      // TODO Move that in a service
+      const enhanceFormData: FormData = new FormData();
+      enhanceFormData.append('image', element.files[0], element.files[0].name);
+      axios
+        .post('http://localhost:3334/api/enhance', enhanceFormData, {
+          params: {
+            // TODO Create a constant for this
+            grayscale: true,
+            threshold: 216,
+            left: 220,
+            top: 135,
+            width: 575,
+            height: 1145,
+            invert: true,
+            toBuffer: true,
+          },
+        })
+        .then((response) => {
+          const ocrSearchParams: URLSearchParams = new URLSearchParams();
+          ocrSearchParams.append('base64', response.data);
+          axios
+            .post('http://localhost:3333/api/ocrFromBase64', ocrSearchParams, {
+              params: {
+                language: 'fra',
+              },
+            })
+            .then((response) => {
+              // TODO Add all regexs
+              const regexs = [
+                '(-* *[0-9]+) *(Intelligence)',
+                '(-* *[0-9]+) *(Force)',
+                '(-* *[0-9]+) *(Agilité)',
+                '(-* *[0-9]+) *(Chance)',
+                '(-* *[0-9]+) *(Vitalité)',
+                '(-* *[0-9]+) *(Sagesse)',
+                // TODO Filter out when it's weapon's AP cost
+                '(-* *[0-9]+) *(PA)',
+                // TODO Filter out when it's weapon added damages
+                // TODO Make sure to distinguish from base damage weapons
+                '(-* *[0-9]+) *(Dommages)',
+                '(-* *[0-9]+) *(Invocation)',
+                '(-* *[0-9]+) *(Fuite)',
+              ];
+              axios
+                .post('http://localhost:3335/api/getResultsFromRegexs', {
+                  message: response.data,
+                  regexs: regexs,
+                })
+                .then((response) => {
+                  this.parsedText = '';
+                  regexs.map((regex) => {
+                    const match = response.data[regex];
+                    // TODO Define what we want to do with empty results
+                    if (this.parsedText) {
+                      this.parsedText += match ? `${match[0]}\n` : '';
+                    } else {
+                      this.parsedText = match ? `${match[0]}\n` : '';
+                    }
+                  });
+                })
+                .catch((error) => {
+                  console.log(error);
+                });
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  };
+}
--- apps/crystalize/src/app/modules/frostbolt/frostbolt.module.ts
+++ apps/crystalize/src/app/modules/frostbolt/frostbolt.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { FileUploaderComponent } from './components/file-uploader/file-uploader.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [FileUploaderComponent],
+  imports: [CommonModule],
+  exports: [FileUploaderComponent],
 })
-export class FrostboltModule { }
+export class FrostboltModule {}
