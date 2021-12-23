e7d75c64156fa21372cbe2ea924c50a539b5b809
Frederick Gibeau <frederick.gibeau@nventive.com>
2021-12-22 19:06:55 -0500

    Add pictures

---
+++ apps/crystalize/src/app/modules/thunderstorm/constants/pictures.ts
@@ -0,0 +1,388 @@
+import { PictureQuiz } from '../interfaces/picture.interface';
+
+export const PictureQuizes: PictureQuiz[] = [
+  {
+    timer: { seconds: 20 },
+    title: 'Connais-tu tes membres_?',
+    questions: [''],
+    answers: ['Marilou'],
+    pictureUrl:
+      'https://drive.google.com/uc?export=view&id=1B1rlnYf77uEudf6BTuWs74Wcn2fHevRz',
+  },
+  {
+    timer: { seconds: 20 },
+    title: 'Connais-tu tes membres_?',
+    questions: [''],
+    answers: ['Marielle'],
+    pictureUrl:
+      'https://drive.google.com/uc?export=view&id=1SxNP_3uZL5Uq-WKhLvz-NYKwKLxavtMR',
+  },
+  {
+    timer: { seconds: 20 },
+    title: 'Connais-tu tes membres_?',
+    questions: [''],
+    answers: ['Catherine'],
+    pictureUrl:
+      'https://drive.google.com/uc?export=view&id=13HL5TKZ70hdaMZIYO6cn8lUNqvOIvhq6',
+  },
+  {
+    timer: { seconds: 20 },
+    title: 'Connais-tu tes membres_?',
+    questions: [''],
+    answers: ['Felix'],
+    pictureUrl:
+      'https://drive.google.com/uc?export=view&id=1CZmrrOlQBPvXTOEoAT6TZHpsugIVkjV0',
+  },
+  {
+    timer: { seconds: 20 },
+    title: 'Connais-tu tes membres_?',
+    questions: [''],
+    answers: ['Catherine'],
+    pictureUrl:
+      'https://drive.google.com/uc?export=view&id=1O_4Aix78wSfoH_VHfsEcT_bY60RaGuO1',
+  },
+  {
+    timer: { seconds: 20 },
+    title: 'Connais-tu tes membres_?',
+    questions: [''],
+    answers: ['Felix'],
+    pictureUrl:
+      'https://drive.google.com/uc?export=view&id=1Kc2eEqzNqvAVrCh3z_4Y_QWbaB_64eG9',
+  },
+  {
+    timer: { seconds: 20 },
+    title: 'Connais-tu tes membres_?',
+    questions: [''],
+    answers: ['Marielle'],
+    pictureUrl:
+      'https://drive.google.com/uc?export=view&id=1gW-pUJ_FJT0gKVFoUgsiAqHcDLnDtwIg',
+  },
+  {
+    timer: { seconds: 20 },
+    title: 'Connais-tu tes membres_?',
+    questions: [''],
+    answers: ['Yvon'],
+    pictureUrl:
+      'https://drive.google.com/uc?export=view&id=1ZX_fKT5Ohv33Cu0-OJLyvurdgnQwJ2ar',
+  },
+  {
+    timer: { seconds: 20 },
+    title: 'Connais-tu tes membres_?',
+    questions: [''],
+    answers: ['Catherine'],
+    pictureUrl:
+      'https://drive.google.com/uc?export=view&id=185FMlj-NVxAqNabMEIGdvDt1XDE98RiP',
+  },
+  {
+    timer: { seconds: 20 },
+    title: 'Connais-tu tes membres_?',
+    questions: [''],
+    answers: ['Jean-Philippe D.'],
+    pictureUrl:
+      'https://drive.google.com/uc?export=view&id=1TZS3H9F3tJyE5_x_xBa8Woz9WDNRI1V-',
+  },
+  {
+    timer: { seconds: 20 },
+    title: 'Connais-tu tes membres_?',
+    questions: [''],
+    answers: ['Jean-Philippe D.'],
+    pictureUrl:
+      'https://drive.google.com/uc?export=view&id=18gn_lH0FmHCAEpQsghiBtXAyDlvsx5Qd',
+  },
+  {
+    timer: { seconds: 20 },
+    title: 'Connais-tu tes membres_?',
+    questions: [''],
+    answers: ['Yvon'],
+    pictureUrl:
+      'https://drive.google.com/uc?export=view&id=1OXxrZBc-it-MvvDuXF6cnb0O_OIzRwXO',
+  },
+  {
+    timer: { seconds: 20 },
+    title: 'Connais-tu tes membres_?',
+    questions: [''],
+    answers: ['Catherine'],
+    pictureUrl:
+      'https://drive.google.com/uc?export=view&id=1-81OxaThRiSy_yQYBa2ebRZoJQMzNG5r',
+  },
+  {
+    timer: { seconds: 20 },
+    title: 'Connais-tu tes membres_?',
+    questions: [''],
+    answers: ['Yvon et Dominique'],
+    pictureUrl:
+      'https://drive.google.com/uc?export=view&id=1ph3bKKE-RRQb-V9Jx0v8tTYHFh7LSbl5',
+  },
+  {
+    timer: { seconds: 20 },
+    title: 'Connais-tu tes membres_?',
+    questions: [''],
+    answers: ['Jean-Philippe G.'],
+    pictureUrl:
+      'https://drive.google.com/uc?export=view&id=11g4tfriRMOeNDh9jyqhhEQdKY_ZhOOuF',
+  },
+  {
+    timer: { seconds: 20 },
+    title: 'Connais-tu tes membres_?',
+    questions: [''],
+    answers: ['Frederick'],
+    pictureUrl:
+      'https://drive.google.com/uc?export=view&id=1N88hCjDVF0zXWhfMeIvcd1CPmu9RV-xD',
+  },
+  {
+    timer: { seconds: 20 },
+    title: 'Connais-tu tes membres_?',
+    questions: [''],
+    answers: ['Marielle'],
+    pictureUrl:
+      'https://drive.google.com/uc?export=view&id=13-yrIQ_j3fZUst_IfeMijNUDaaYsX5dx',
+  },
+  {
+    timer: { seconds: 20 },
+    title: 'Connais-tu tes membres_?',
+    questions: [''],
+    answers: ['Jean-Philippe G.'],
+    pictureUrl:
+      'https://drive.google.com/uc?export=view&id=1X4n7yfO1AW75n1c4nqguMHMzB14tq0Iq',
+  },
+  {
+    timer: { seconds: 20 },
+    title: 'Connais-tu tes membres_?',
+    questions: [''],
+    answers: ['Arianne'],
+    pictureUrl:
+      'https://drive.google.com/uc?export=view&id=1i2fWGFrhjjNBT3zNSTIsCJ-WoI3psut9',
+  },
+  {
+    timer: { seconds: 20 },
+    title: 'Connais-tu tes membres_?',
+    questions: [''],
+    answers: ['Rose'],
+    pictureUrl:
+      'https://drive.google.com/uc?export=view&id=1LpHagK5_kKPd7rMjhb4b0PzbawRxHzWu',
+  },
+  {
+    timer: { seconds: 20 },
+    title: 'Connais-tu tes membres_?',
+    questions: [''],
+    answers: ['Yvon'],
+    pictureUrl:
+      'https://drive.google.com/uc?export=view&id=1flwGVePS_pJVzq_niQFJIeWBWOuEZPjT',
+  },
+  {
+    timer: { seconds: 20 },
+    title: 'Connais-tu tes membres_?',
+    questions: [''],
+    answers: ['Jean-Philippe G.'],
+    pictureUrl:
+      'https://drive.google.com/uc?export=view&id=1catf8Qc2H_OhToH6FsXnwO9Y5s8b8dOq',
+  },
+  {
+    timer: { seconds: 20 },
+    title: 'Connais-tu tes membres_?',
+    questions: [''],
+    answers: ['Jean-Philippe D.'],
+    pictureUrl:
+      'https://drive.google.com/uc?export=view&id=1vo6y96UY-1yBHYf2Xj7uwkNLQdAViCPQ',
+  },
+  {
+    timer: { seconds: 20 },
+    title: 'Connais-tu tes membres_?',
+    questions: [''],
+    answers: ['Les Jean-Philippe'],
+    pictureUrl:
+      'https://drive.google.com/uc?export=view&id=1m3p6O4UfShsorO5LPmBMFJE8LQLOrd4V',
+  },
+  {
+    timer: { seconds: 20 },
+    title: 'Connais-tu tes membres_?',
+    questions: [''],
+    answers: ['Jean-Philippe G.'],
+    pictureUrl:
+      'https://drive.google.com/uc?export=view&id=1z-W2IOA0IXUDJw2LY5M2EKkxJDwmKwlG',
+  },
+  {
+    timer: { seconds: 20 },
+    title: 'Connais-tu tes membres_?',
+    questions: [''],
+    answers: ['Arianne'],
+    pictureUrl:
+      'https://drive.google.com/uc?export=view&id=1LGUBwsVEpWHmKuqMWwhviw17as5ojC68',
+  },
+  {
+    timer: { seconds: 20 },
+    title: 'Connais-tu tes membres_?',
+    questions: [''],
+    answers: ['Arianne'],
+    pictureUrl:
+      'https://drive.google.com/uc?export=view&id=1_OhGaMjXTYPQgf4XN81IWncmeuQ8jkd3',
+  },
+  {
+    timer: { seconds: 20 },
+    title: 'Connais-tu tes membres_?',
+    questions: [''],
+    answers: ['Marilou'],
+    pictureUrl:
+      'https://drive.google.com/uc?export=view&id=1AWBFaClRQ1nvQ5gLhwfYL1PpjnxI_V1b',
+  },
+  {
+    timer: { seconds: 20 },
+    title: 'Connais-tu tes membres_?',
+    questions: [''],
+    answers: ['Yvon'],
+    pictureUrl:
+      'https://drive.google.com/uc?export=view&id=16DlxgXNan49CmV3XJhol8p3749MxG-fy',
+  },
+  {
+    timer: { seconds: 20 },
+    title: 'Connais-tu tes membres_?',
+    questions: [''],
+    answers: ['Jean-Philippe G.'],
+    pictureUrl:
+      'https://drive.google.com/uc?export=view&id=1BDFjdEPCau7fR_3TkJeTncbK0PpicCGK',
+  },
+  {
+    timer: { seconds: 20 },
+    title: 'Connais-tu tes membres_?',
+    questions: [''],
+    answers: ['Felix'],
+    pictureUrl:
+      'https://drive.google.com/uc?export=view&id=1Uv_D1kSHvfczh7vHVeVOIyRfzW0OuEKq',
+  },
+  {
+    timer: { seconds: 20 },
+    title: 'Connais-tu tes membres_?',
+    questions: [''],
+    answers: ['Marielle'],
+    pictureUrl:
+      'https://drive.google.com/uc?export=view&id=1JdzRkKGvGYaW9kzZqL-T7pYoSyB1MfW0',
+  },
+  {
+    timer: { seconds: 20 },
+    title: 'Connais-tu tes membres_?',
+    questions: [''],
+    answers: ['Marielle'],
+    pictureUrl:
+      'https://drive.google.com/uc?export=view&id=1EMukf4oNMpQ-tSmjsOvo2GfISgqO0Tuk',
+  },
+  {
+    timer: { seconds: 20 },
+    title: 'Connais-tu tes membres_?',
+    questions: [''],
+    answers: ['Arianne'],
+    pictureUrl:
+      'https://drive.google.com/uc?export=view&id=1cTP7-xne4LdwOKPVkLTNMkczPiTpd_FW',
+  },
+  {
+    timer: { seconds: 20 },
+    title: 'Connais-tu tes membres_?',
+    questions: [''],
+    answers: ['Jean-Philippe D.'],
+    pictureUrl:
+      'https://drive.google.com/uc?export=view&id=1HWKkyPUUGfJsc2iFb_lkIfw758RENiFB',
+  },
+  {
+    timer: { seconds: 20 },
+    title: 'Connais-tu tes membres_?',
+    questions: [''],
+    answers: ['Jean-Philippe D.'],
+    pictureUrl:
+      'https://drive.google.com/uc?export=view&id=1hCOtYIhYLx_iqXs4LeMBJdh2BRrR1FP2',
+  },
+  {
+    timer: { seconds: 20 },
+    title: 'Connais-tu tes membres_?',
+    questions: [''],
+    answers: ['Marilou'],
+    pictureUrl:
+      'https://drive.google.com/uc?export=view&id=13C7Cwn6SJkvRZxyYm_LlkqawspLNFbB9',
+  },
+  {
+    timer: { seconds: 20 },
+    title: 'Connais-tu tes membres_?',
+    questions: [''],
+    answers: ['Jean-Philippe G.'],
+    pictureUrl:
+      'https://drive.google.com/uc?export=view&id=1SnNBeNVS5veSeJcB4eoL225jFa6Pqpj1',
+  },
+  {
+    timer: { seconds: 20 },
+    title: 'Connais-tu tes membres_?',
+    questions: [''],
+    answers: ['Catherine'],
+    pictureUrl:
+      'https://drive.google.com/uc?export=view&id=1I885jVSC2RPKMyWqBGxzK4yeH6Bk3CrD',
+  },
+  {
+    timer: { seconds: 20 },
+    title: 'Connais-tu tes membres_?',
+    questions: [''],
+    answers: ['Yvon'],
+    pictureUrl:
+      'https://drive.google.com/uc?export=view&id=1DhQ3gVQJITJ_pNhmvkAJnccXYUeIrZeT',
+  },
+  {
+    timer: { seconds: 20 },
+    title: 'Connais-tu tes membres_?',
+    questions: [''],
+    answers: ['Thierry'],
+    pictureUrl:
+      'https://drive.google.com/uc?export=view&id=1a4eBEJEQKca02VSu0-3VVuG9cihF1iqP',
+  },
+  {
+    timer: { seconds: 20 },
+    title: 'Connais-tu tes membres_?',
+    questions: [''],
+    answers: ['Charlotte'],
+    pictureUrl:
+      'https://drive.google.com/uc?export=view&id=1zVgKTrV1eZbRcaFsEc7XNY6fCbmb1Lvr',
+  },
+  {
+    timer: { seconds: 20 },
+    title: 'Connais-tu tes membres_?',
+    questions: [''],
+    answers: ['Arianne'],
+    pictureUrl:
+      'https://drive.google.com/uc?export=view&id=1KZGRjynuX-Ms_38GVwTcDPVzjB3xSN3S',
+  },
+  {
+    timer: { seconds: 20 },
+    title: 'Connais-tu tes membres_?',
+    questions: [''],
+    answers: ['Jean-Philippe D.'],
+    pictureUrl:
+      'https://drive.google.com/uc?export=view&id=1dobOwmLMp1Og0ARRAK8z9nL5oAGIX6Ta',
+  },
+  {
+    timer: { seconds: 20 },
+    title: 'Connais-tu tes membres_?',
+    questions: [''],
+    answers: ['Arianne'],
+    pictureUrl:
+      'https://drive.google.com/uc?export=view&id=1xSnhjrqkmp8FsScAjGMuFKZ4aA0TG2Dk',
+  },
+  {
+    timer: { seconds: 20 },
+    title: 'Connais-tu tes membres_?',
+    questions: [''],
+    answers: ['Marilou'],
+    pictureUrl:
+      'https://drive.google.com/uc?export=view&id=1X4wU0Mwl6yvOoPRTQeee_Av4JC1BfRQ_',
+  },
+  {
+    timer: { seconds: 20 },
+    title: 'Connais-tu tes membres_?',
+    questions: [''],
+    answers: ['Charlotte'],
+    pictureUrl:
+      'https://drive.google.com/uc?export=view&id=1gpuUxU2G_kSOHRUVC0Ct1mrYi0ypf4o0',
+  },
+  {
+    timer: { seconds: 20 },
+    title: 'Connais-tu tes membres_?',
+    questions: [''],
+    answers: ['Philip'],
+    pictureUrl:
+      'https://drive.google.com/uc?export=view&id=1i9netRkzWIyDHzFBjfXeZNUjOUSDRWKw',
+  },
+];
